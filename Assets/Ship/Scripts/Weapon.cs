5e42c9963c6c368f2b22905592840f074feb1dba
Pierre Demessence <pierre.demessence@gmail.com>
2017-11-02 03:27:58 +0100

    Multiple bullets by weapon

--- Assets/Ship/Scripts/Weapon.cs
+++ Assets/Ship/Scripts/Weapon.cs
@@ -3,8 +3,10 @@
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private PoolableEntity _bullet;
-    [SerializeField] private float _rof;
+    [SerializeField] private int _bulletAmount;
+    [SerializeField] private float _bulletSpacing = 1;
     private float _lastFire;
+    [SerializeField] private float _rof;
 
     public float Rof
     {
@@ -16,8 +18,15 @@ public class Weapon : MonoBehaviour
     {
         if (!(Time.time - _lastFire > 1 / Rof)) return false;
 
-        var bullet = ObjectPool.Instance.GetPooledObject(_bullet.GetType());
-        bullet.transform.position = transform.position;
+        var space = _bullet.GetComponent<SpriteRenderer>().bounds.size.x * _bulletSpacing;
+        var left = transform.position.x - (_bullet.GetComponent<SpriteRenderer>().bounds.size.x + space) * ((_bulletAmount - 1) / 2f);
+        for (var i = 0; i < _bulletAmount; ++i)
+        {
+            var x = left + (_bullet.GetComponent<SpriteRenderer>().bounds.size.x + space) * i;
+            var bullet = ObjectPool.Instance.GetPooledObject(_bullet.GetType());
+            bullet.transform.position = new Vector3(x, transform.position.y, transform.position.y);
+        }
+        Debug.Log(_bullet.GetComponent<SpriteRenderer>().bounds.size);
         _lastFire = Time.time;
         return true;
     }
